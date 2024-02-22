4c678aeb312953adefbe0230ecc95fa5b3c26917
mohamedbfr <mohamedyonko3@gmail.com>
2024-02-22 15:31:02 +0100

    doc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    '@fortawesome/fontawesome-free/css/all.css'
-  ],
-  plugins: [
-    { src: '~/plugins/html2pdf.js', mode: 'client' }
-  ]
-})
+  css: ["@fortawesome/fontawesome-free/css/all.css"],
+});
